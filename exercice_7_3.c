5d4ab1774c8f3c450933f2a022f5121895a586f8
Samuel SCHNEPP <samuel.schnepp@gmail.com>
2024-08-21 11:21:17 +0200

    -

--- exercice_7_3.c
+++ exercice_7_3.c
@@ -2,5 +2,24 @@
 
 int main(){
 
+	int t[20];
+	int i ;
+	int max ;
+	int position ;
+	position = 0 ;
+	max = 0 ;
+	for ( i = 0 ; i < 20 ; i++ ){
+		printf ("Donnez un nombre entier : \n") ;
+		scanf (" %d" , &t[i] );
+	}
+	for ( i = 0 ; i < 20 ; i++ ){
+		if (t[i] > max ){
+		       	position = i ;
+			max = t[i] ;
+		}
+	}
+	printf ("La position de la plus grande valeur est  %d \n", position + 1 ) ;
+	return 7 ;
 }
+			
 
